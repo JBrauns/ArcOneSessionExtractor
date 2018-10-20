b8661ed99683a7173167344391071f46bf1ba620
JBrauns <joshua_brauns@live.de>
2018-10-20 21:02:37 +0200

    Update the file extraction and parameter processing

--- src/stdp_extractor.py
+++ src/stdp_extractor.py
@@ -1,89 +1,77 @@
 import re
 import argparse
 import math
+import os, sys
 
-# NOTE(joshua): wordline - 0, bitline - 1, resistance - 2,
-# amplitude - 3, pulse_width - 4, tag - 5, read_tag - 6, read_voltage - 7
+matplotlibFound = False
+try:
+    import matplotlib
+    matplotlibFound = True
+except ImportError:
+    print("Matplotlib not found on this system")
 
-class Stdp_Test_Point:
-    def __init__(self, pos_init, r_init, amp_init, pw_init, v_init):
-        # NOTE(joshua): Wordline, bitline
-        self.pos = pos_init
-        self.r = r_init
-        self.amp = amp_init
-        self.pw = pw_init
-        self.v = v_init
+numpyFound = False
+try:
+    import numpy
+    numpyFound = True
+except ImportError:
+    print("Numpy not found on this system")
 
-class Stdp_Block:
-    def __init__(self, index, wl, bl, r, amp, pw, tag, read_tag, v):
-        self.index = index
-        self.stdb_begin = Stdp_Test_Point([wl, bl], r, amp, pw, v)
+if(matplotlibFound and numpyFound):
+    import matplotlib.pyplot as pyPlot
 
-        self.test_points_after = []
-        self.test_points_after_dt = []
-        self.test_points_before = []
-        self.test_points_before_dt = []
+def plotToImage(x, y, path, fileName, **kwargs):
+    if(not(matplotlibFound and numpyFound)):
+        print("Can't plotToImage for %s" % (fileName))
+        return
+    else:
+        factor = 0.7
+        fig = pyPlot.figure(figsize=[14*factor, 8.65*factor])
 
-    def end(self, wl, bl, r, amp, pw, tag, read_tag, v):
-        self.stdb_end = Stdp_Test_Point([wl, bl], r, amp, pw, v)
-        
-    def add_test_point(self, wl, bl, r, amp, pw, tag, read_tag, v):
-        pos = [wl, bl]
-        
-        dt_before_match = re.search("(?<=dt=)-?\d.\d+(?=\ before)", tag)
-        if(dt_before_match):
-            dt_before = dt_before_match.group(0)
-            if(dt_before != ""):
-                self.test_points_before_dt.append(dt_before)
-                self.test_points_before.append(Stdp_Test_Point(pos, r, amp, pw, v))
-        else:
-            dt_after_match = re.search("(?<=dt=)-?\d.\d+(?=\ after)", tag)
-            if(dt_after_match):
-                dt_after = dt_after_match.group(0)
-                self.test_points_after_dt.append(dt_after)
-                self.test_points_after.append(Stdp_Test_Point(pos, r, amp, pw, v))
+        xPy = numpy.asarray(x)
+        yPy = numpy.asarray(y)
 
-    def format_content(self, sep, logarithmic):
-        dest = ""
-        test_point_count = len(self.test_points_after)
+        gridOn = False
+        lineStyle = '.'
+        fileFormat = 'svg'
+        for(key, value) in kwargs.items():
+            if(key == 'title'):
+                pyPlot.title(kwargs['title'])
+            if(key == 'ylabel'):
+                pyPlot.ylabel(kwargs['ylabel'])
+            if(key == 'xlabel'):
+                pyPlot.xlabel(kwargs['xlabel'])
+            if(key == 'grid_on'):
+                gridOn = True
+            if(key == 'line_style'):
+                lineStyle = value
+            if(key == 'format'):
+                fileFormat = value
 
-        print("In format_content %d count %d" % (self.index, test_point_count))
+        pyPlot.plot(xPy, yPy, lineStyle)
+        # pyPlot.ylim((-0.15, 0.15))
+        pyPlot.grid(linestyle='dotted')
+        # pyPlot.yticks(numpy.arange(-0.15, 0.15, step=0.1))
 
-        if(len(self.test_points_after) == len(self.test_points_before)):
-            if(len(self.test_points_after) == len(self.test_points_after_dt) and
-               len(self.test_points_before) == len(self.test_points_before_dt)):
-                
-                for test_point_index in range(0, test_point_count):
-                    tp_before = self.test_points_before[test_point_index]
-                    tp_after = self.test_points_after[test_point_index]
+        totalPath = ".".join((os.path.join(os.path.abspath(path), fileName), fileFormat))
+        # pyPlot.gca().set_position([0, 0, 1, 1])
+        pyPlot.savefig(totalPath, format=fileFormat)
 
-                    r_after = float(tp_after.r)
-                    r_before = float(tp_before.r)
-                    if((r_after != float('inf')) and (r_before != float('inf'))):
-                        dest += self.test_points_after_dt[test_point_index].replace(".", ",") + sep
+class enum :
+    def __init__(self, attributes):
+        attributeIndex = int(0)
+        for attribute in attributes:
+            setattr(self, attribute, attributeIndex)
+            attributeIndex += 1
 
-                        r_delta = r_before - r_after
-                        r_0 = min(r_after, r_before)
-
-                        r_out = r_delta / r_0
-                        if(logarithmic):
-                            sign = 1.0 if (r_out >= 0) else -1.0
-                            r_out = sign*math.log(r_out*sign)
-                        
-                        dest += str(r_delta).replace(".", ",") + sep
-                        dest += str(r_0).replace(".", ",") + sep
-                        dest += str(r_out).replace(".", ",")
-                        dest += "\n"
-            else:
-                print("Unqueal count of tp and tp_delta: %after=d,%d before=%d,%d" %
-                      (len(self.test_points_after), len(self.test_points_after_dt) ,
-                       len(self.test_points_after), len(self.test_points_before_dt)))
-        else:
-            print("Unequal count of before and after testpoints: %d,%d" %
-                  (len(self.test_points_after), len(self.test_points_before)))
-
-        return(dest)
-            
+stdp_data_e = enum(['wordline',
+                    'bitline',
+                    'resistance',
+                    'amplitude',
+                    'pulsewidth',
+                    'tag',
+                    'readtag',
+                    'readvoltage'])
 
 def get_file_content(path):
     with open(path, "r") as src:
@@ -102,47 +90,105 @@ parser.add_argument("-s", dest="src", help="Source of stdp data")
 args = parser.parse_args()
 
 print("Args: src=%s\ndest=%s" % (args.src, args.dest))
-src_lines = get_file_content(args.src)
+fileContent = get_file_content(args.src)
 
-src_line_index = 0
-parse_block = False
+def getStdpBlock(data, rowEntries, rowEntriesLast):
+    result = {'dt':None, 'r0':None, 'r1':None, 'a':None, 'pw':None}
 
-stdp_block_current_index = 0
-stdp_blocks = []
-stdp_count = 0
-for src_line in src_lines:
-    # NOTE(joshua): wordline - 0, bitline - 1, resistance - 2,
-    # amplitude - 3, pulse_width - 4, tag - 5, read_tag - 6, read_voltage - 7
-    cells = src_line.split(",")
+    result['r0'] = rowEntries[stdp_data_e.resistance]
+    result['r1'] = rowEntriesLast[stdp_data_e.resistance]
 
-    if(len(cells) >= 8):
-        tag = cells[5]
-        if(tag == "stdp_s"):
-            parse_block = True
-            stdp_blocks.append(Stdp_Block(stdp_block_current_index, cells[0], cells[1], cells[2], cells[3],
-                                          cells[4], cells[5], cells[6], cells[7]))
-            # print("STDP block start @line: %d" % src_line_index)
-            stdp_count = src_line_index
+    result['a'] = rowEntries[stdp_data_e.amplitude]
+    result['pw'] = rowEntries[stdp_data_e.pulsewidth]
+
+    groups = re.findall("(-?\\d+.\\d+)", rowEntries[stdp_data_e.tag])
+    assert (len(groups) == 1), "Unexpected ammount of deltas found: %d" % len(groups)
+    for group in groups:
+        result['dt'] = float(group)
+
+    data.append(result)
+        
+class stdp_block:
+    def __init__(self):
+        self.begin = {'wl':0, 'bl':0, 'rv':0}
+        self.end = {}
+        self.data = []
+
+def floatInRange(value):
+    result = (value != float('+inf')) and (value != float('-inf')) and (value != float('nan'))
+    return result
+        
+stdpSetIndex = 0
+stdpSets = [stdp_block()]
+
+inStdpBlock = False
+lineIndex = 0
+rowEntriesLast = None
+for line in fileContent:
+    rowEntries = line.split(",")
+    
+    tag = ""
+    if(len(rowEntries) > stdp_data_e.tag):
+        tag = rowEntries[stdp_data_e.tag]
+
+    if(tag == "stdp_e"):
+        inStdpBlock = False
+        stdpSets[stdpSetIndex].end = ()
+        stdpSetIndex += 1
+        
+        stdpSets.append(stdp_block())
+    elif((tag == "stdp_s") or inStdpBlock):
+        stdpSets[stdpSetIndex].begin['wl'] = rowEntries[stdp_data_e.wordline]
+        stdpSets[stdpSetIndex].begin['bl'] = rowEntries[stdp_data_e.bitline]
+        stdpSets[stdpSetIndex].begin['rv'] = rowEntries[stdp_data_e.readvoltage]
+        if(inStdpBlock):
+            if(re.search("(after)", rowEntries[stdp_data_e.tag].strip())):
+                getStdpBlock(stdpSets[stdpSetIndex].data, rowEntries, rowEntriesLast)
+                
+        inStdpBlock = True
+
+    lineIndex += 1
+    rowEntriesLast = rowEntries
+
+stdpSetIndex = 0
+for stdpSet in stdpSets:
+    # print("STDP_SET: %d" % stdpSetIndex)
+    sortedData = sorted(stdpSet.data, key=lambda k: k['dt'])
+
+    csvContent = ""
+    dtData = []
+    valuesY = []
+    dataSetIndex = 0
+    for dataSet in sortedData:
+        r0 = float(dataSet['r0'])
+        r1 = float(dataSet['r1'])
+        if(floatInRange(r0) and floatInRange(r1) and (r0 != r1)):
+            xValue = float(dataSet['dt']) * (1000**2)
+            dtData.append(xValue)
             
-        elif(tag == "stdp_e"):
-            parse_block = False
-            stdp_blocks[stdp_block_current_index].end(cells[0], cells[1], cells[2], cells[3],
-                                                      cells[4], cells[5], cells[6], cells[7])
-            stdp_count = src_line_index - stdp_count
-            #print("STDP block end @line: %d" % src_line_index)
-            #print("STDP count: %d" % stdp_count)
-            stdp_block_current_index += 1
+            valueSign = -1 if (r1 > r0) else 1
+            valueMax = max(r0, r1)
+            valueMin = min(r0, r1)
+            valueNorm = valueMin
+            valueDelta = (valueMax - valueMin)
+            yValue = (valueDelta / valueNorm)*valueSign
+            valuesY.append(yValue)
 
-        elif(parse_block):
-            stdp_blocks[stdp_block_current_index].add_test_point(cells[0], cells[1], cells[2], cells[3],
-                                                                 cells[4], cells[5], cells[6], cells[7])
-            
-    src_line_index += 1
+            csvContent += ";".join((str(xValue), str(yValue), "\n"))
 
-print("STDP block count: %d" % (len(stdp_blocks)))
-
-for block_index in range(0, len(stdp_blocks)):
-    path_out = args.dest.replace(".csv", ("_" + str(block_index) + ".csv"))
-    write_file_content(path_out, stdp_blocks[block_index].format_content(";", False))
-    print("File %s written!" % path_out)
+    print("Plot image number \"%d\"" % (stdpSetIndex))
+    fileFormats = ['svg', 'png']
+    for fileFormat in fileFormats:
+        plotToImage(dtData, valuesY, "..\\img", "test_"+str(stdpSetIndex),
+                    title="STDP Set "+str(stdpSetIndex)+";Probe C3_3.5k$\Omega$", ylabel=r'$\Delta G/G_0$',
+                    xlabel=r'$\Delta t (\mu s)$', format=fileFormat)
+    pathOut = os.path.abspath("..\img") + "\\test_{0}.csv".format(stdpSetIndex) 
+    write_file_content(pathOut, csvContent)
+    
+    stdpSetIndex +=1
+    
+#for block_index in range(0, len(stdp_blocks)):
+#    path_out = args.dest.replace(".csv", ("_" + str(block_index) + ".csv"))
+#    write_file_content(path_out, stdp_blocks[block_index].format_content(";", False))
+#    print("File %s written!" % path_out)
 
